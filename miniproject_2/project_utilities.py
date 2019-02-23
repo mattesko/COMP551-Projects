0ed960892a691580efa4666d5aedb0d862a0a913
Matthew Lesko <mlesko1996@gmail.com>
2019-02-22 19:01:56 -0500

    Plot confusion matrix for best performing classifier

--- miniproject_2/project_utilities.py
+++ miniproject_2/project_utilities.py
@@ -1,5 +1,8 @@
 import os
 import re
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 
 """
 Contains helper functions for importing train and test data
@@ -61,3 +64,34 @@ def import_test_data(path='test'):
                 })
 
     return test_data
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues,
+                          figsize=(10,10)):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
